77b31f37b3ab44bc9cb2621b45b6e8ea6d27fafd
Guilherme Cunha <cunha0889@gmail.com>
2024-05-21 21:05:01 -0300

    base da navbar

--- src/app/components/layot/principal/principal.component.ts
+++ src/app/components/layot/principal/principal.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component'; 
 import { MenuComponent } from '../menu/menu.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [MenuComponent, RouterOutlet],
+  imports: [NavbarComponent, MenuComponent, RouterOutlet],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.scss'
 })
