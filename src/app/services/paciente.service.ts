d6bd9b6eb73f6bbdcb05b5242977c5b3c1dddb50
Bruno Vieira Nobre <104945877+BrunoV7@users.noreply.github.com>
2024-06-06 03:54:27 -0300

    Melhorias esteticas

--- src/app/services/paciente.service.ts
+++ src/app/services/paciente.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Paciente } from '../models/paciente';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +13,11 @@ export class PacienteService {
   http = inject(HttpClient);
   
   API = "http://localhost:8080/api/paciente";
+  CATAPI = "https://api.thecatapi.com/v1/breeds";
+  DOGAPI = "https://api.thedogapi.com/v1/breeds";
 
   constructor() { }
+
   listAll(): Observable<Paciente[]>{
     return this.http.get<Paciente[]>(this.API+"/listAll");
   }
@@ -32,5 +36,17 @@ export class PacienteService {
 
   findById(id: number): Observable<Paciente>{
     return this.http.get<Paciente>(this.API+"/findById/"+id);
+  }
+
+  getRacas(): Observable<string[]>{
+    return this.http.get<any[]>(this.CATAPI).pipe(
+      map(breeds => breeds.map(breed => breed.name))
+    );
+  }
+
+  getRacasDog(): Observable<string[]>{
+    return this.http.get<any[]>(this.DOGAPI).pipe(
+      map(breeds => breeds.map(breed => breed.name))
+    );
   }
 }
