5b7401218453d93f9171128fb25365a085949f30
rorxtto <pedrohenriqueroratto@gmail.com>
2024-06-19 21:21:25 -0300

    tela de cadastro

--- src/app/components/layot/login/login.component.ts
+++ src/app/components/layot/login/login.component.ts
@@ -2,16 +2,63 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
+<<<<<<< Updated upstream
+=======
+import { Login } from '../../../auth/login';
+import { LoginService } from '../../../auth/login.service';
+import { CommonModule } from '@angular/common';
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 
 export class LoginComponent {
+<<<<<<< Updated upstream
+=======
+  login: Login = new Login ();
+
+  loginService = inject(LoginService);
+  router = inject(Router);
+
+  logar(){
+    console.log(this.login);
+    this.loginService.logar(this.login).subscribe({
+      next: token => {
+        console.log(token);
+        this.loginService.addToken(token);
+        this.router.navigate(['/admin/dashboard']);
+      },
+      error: erro => {
+        Swal.fire({
+          title: "Erro",
+          confirmButtonColor: "",
+          confirmButtonText: "Tentar novamente",
+          text: "Login ou senha incorreta.",
+          icon: "error"
+        });
+      }
+    });
+  }
+
+  modoLogin: boolean = true; // Variável para controlar o modo de exibição inicial (true para login, false para cadastro)
+
+  // Outros métodos e propriedades do seu componente
+
+  toggleModo(): void {
+    this.modoLogin = !this.modoLogin; // Alternar entre login e cadastro ao clicar no botão "Cadastre-se"
+  }
+
+  reloadPage(): void {
+    window.location.reload(); // Função para recarregar a página
+  }
+  
+  /*
+>>>>>>> Stashed changes
   login!: string;
   senha!: string;
 
