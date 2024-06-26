da8df0c1f13d892c12e50611cf41f3a1f432461d
rorxtto <pedrohenriqueroratto@gmail.com>
2024-06-25 21:03:27 -0300

    cadastro funcionando

--- src/app/components/layot/login/login.component.ts
+++ src/app/components/layot/login/login.component.ts
@@ -2,12 +2,10 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
-<<<<<<< Updated upstream
-=======
 import { Login } from '../../../auth/login';
+import { Usuario } from '../../../auth/usuario';
 import { LoginService } from '../../../auth/login.service';
 import { CommonModule } from '@angular/common';
->>>>>>> Stashed changes
 
 @Component({
   selector: 'app-login',
@@ -18,9 +16,8 @@ import { CommonModule } from '@angular/common';
 })
 
 export class LoginComponent {
-<<<<<<< Updated upstream
-=======
   login: Login = new Login ();
+  usuario: Usuario = new Usuario ();
 
   loginService = inject(LoginService);
   router = inject(Router);
@@ -45,42 +42,30 @@ export class LoginComponent {
     });
   }
 
-  modoLogin: boolean = true; // Variável para controlar o modo de exibição inicial (true para login, false para cadastro)
+  cadastrar(): void {
+    this.loginService.cadastrar(this.usuario).subscribe({
+      next: token => {
+        console.log(token);
+        this.loginService.addToken(token);
+        this.router.navigate(['/admin/dashboard']);
+      },
+      error: (erro: any) => {
+        Swal.fire({
+          title: "Erro",
+          confirmButtonColor: "",
+          confirmButtonText: "Tentar novamente",
+          text: "Erro ao cadastrar usuário.",
+          icon: "error"
+        });
+      }
+    });
+  }
 
-  // Outros métodos e propriedades do seu componente
+  modoLogin: boolean = true; // Variável para controlar o modo de exibição inicial (true para login, false para cadastro)
 
   toggleModo(): void {
     this.modoLogin = !this.modoLogin; // Alternar entre login e cadastro ao clicar no botão "Cadastre-se"
   }
-
-  reloadPage(): void {
-    window.location.reload(); // Função para recarregar a página
-  }
   
-  /*
->>>>>>> Stashed changes
-  login!: string;
-  senha!: string;
-
-  router = inject(Router);
-
-  logar(){
-    if(this.login == "admin" && this.senha == "admin"){
-      Swal.fire({
-        position: "center",
-        icon: "success",
-        showConfirmButton: false,
-        timer: 1500
-      });
-      this.router.navigate(['admin/pacientes']);
-    }else{
-      Swal.fire({
-        title: "Erro",
-        confirmButtonColor: "",
-        confirmButtonText: "Tentar novamente",
-        text: "Login ou senha incorreta.",
-        icon: "error"
-      });
-    }
-  }
 }
+
